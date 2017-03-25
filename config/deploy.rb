93028f1e4c85fc58e0cb2a0c08316b1d7ec731a0
charles <cshao25@gmail.com>
2017-03-25 20:39:05 +1100

    Add puma configs to deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,3 +36,7 @@ set :linked_dirs, fetch(:linked_dirs, []).push("log", "tmp/pids", "tmp/cache", "
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
+
+ set :puma_init_active_record, true
+
+ set :puma_conf, "#{shared_path}/config/puma.rb"
