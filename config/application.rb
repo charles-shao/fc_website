85d8c5df2cac9a0f8d65c748a065e36328104682
charles <cshao25@gmail.com>
2017-03-26 15:33:46 +1100

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,8 @@ module FcWebsite
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    set :assets_prefix, "prepackaged-assets"
-    
+    config.assets.prefix = "prepackaged-assets"
+
     config.before_configuration do
       env_file = File.join(Rails.root, "config", "local_env.yml")
       YAML.load(File.open(env_file)).each do |key, value|
