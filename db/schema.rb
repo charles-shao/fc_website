d4539db57fa7f7c80766705d255b8b0bcaa4e90e
Charles Shao <charles@oneflare.com>
2017-03-31 09:43:11 +1100

    Add job data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170330105608) do
+ActiveRecord::Schema.define(version: 20170330220543) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,13 @@ ActiveRecord::Schema.define(version: 20170330105608) do
     t.string   "action"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+  end
+
+  create_table "jobs", force: :cascade do |t|
+    t.string   "name"
+    t.string   "role"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "notices", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Jobs
+jobs = [
+  { name: "Warrior", role: "Tank" },
+  { name: "Paladin", role: "Tank" },
+  { name: "Monk", role: "DPS" },
+  { name: "Dragoon", role: "DPS" },
+  { name: "Bard", role: "DPS" },
+  { name: "White Mage", role: "Healer" },
+  { name: "Black Mage", role: "DPS" },
+  { name: "Summoner", role: "DPS" },
+  { name: "Scholar", role: "Healer" },
+  { name: "Ninja", role: "DPS" },
+  { name: "Dark Knight", role: "Tank" },
+  { name: "Astrologian", role: "Healer" },
+  { name: "Machinist", role: "DPS" },
+  { name: "Red Mage", role: "DPS" },
+  { name: "Samurai", role: "DPS" }
+]
+
+jobs.each do |job|
+  Job.create(name: job[:name], role: job[:role])
+end
