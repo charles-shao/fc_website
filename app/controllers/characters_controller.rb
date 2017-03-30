a31c91d68236d6412101f64eb11b76f99828b570
charles <cshao25@gmail.com>
2017-03-30 21:48:43 +1100

    Profile updates

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -16,12 +16,31 @@ class CharactersController < ApplicationController
   end
 
   def edit
+    @character = current_user.characters.find_by(id: params[:id])
   end
 
   def update
+    @character = current_user.characters.find_by(id: params[:id])
+
+    if @character.update_attributes(character_params)
+      flash[:success] = "Successfully updated character."
+      redirect_to profiles_path
+    else
+      flash[:danger] = @character.errors.full_messages.to_sentence
+      render :edit
+    end
   end
 
   def destroy
+    @character = current_user.characters.find_by(id: params[:id])
+
+    if @character.destroy
+      flash[:success] = "Successfully deleted #{@character.name}."
+    else
+      flash[:danger] = @character.errors.full_messages.to_sentence
+    end
+
+    redirect_to profiles_path
   end
 
   private
