4c7598a6fee3874ba0e9eb31c437dab9b5d13873
charles <cshao25@gmail.com>
2017-04-02 02:00:46 +1100

    Fix migration

--- db/migrate/20170401144425_rename_static_member.rb
+++ db/migrate/20170401144425_rename_static_member.rb
@@ -1,5 +1,5 @@
 class RenameStaticMember < ActiveRecord::Migration[5.0]
   def change
-    rename_table :static_characters, :static_characters
+    rename_table :static_members, :static_characters
   end
 end
