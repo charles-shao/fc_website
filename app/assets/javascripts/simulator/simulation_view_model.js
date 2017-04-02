96169a9916195e70f4b61376749b4ec4d2d6cd8b
charles <cshao25@gmail.com>
2017-04-03 00:26:13 +1000

    Remove ES6 for

--- app/assets/javascripts/simulator/simulation_view_model.js
+++ app/assets/javascripts/simulator/simulation_view_model.js
@@ -5,26 +5,22 @@ function SimulationViewModel() {
   var self = this;
   // Spells
   self.spells = ko.observableArray([]);
-  self.selectedSpell = ko.observable();
   // Buffs
   self.damageMultiplierAbilities = ko.observableArray([]);
-  self.selectedDamageMultiplierAbility = ko.observable();
 
   self.timeline = new Timeline();
 
   self.addSpellToQueue = function(spell) {
     self.timeline.addToActionQueue(spell);
-  }
+  };
 
   self.addDamageMultiplierToQueue = function(ability) {
-   self.timeline.addToActionQueue(ability);
-  }
+    self.timeline.addToActionQueue(ability);
+  };
 
   self.removeActionFromQueue = function(action) {
-    console.log(action)
-
     self.timeline.removeFromActionQueue(action);
-  }
+  };
 
   // TODO: read from database
   var spellObjects = [
@@ -35,18 +31,16 @@ function SimulationViewModel() {
   ];
 
   var damageMultiplierAbilities = [
-    // { id: "100", name: "Astral Fire I", multiplier: 1.2, duration: 12, cooldown: 0, animationLock: 0 },
-    // { id: "101", name: "Astral Fire II", multiplier: 1.4, duration: 12, cooldown: 0, animationLock: 0 },
-    // { id: "102", name: "Astral Fire III", multiplier: 1.8, duration: 12, cooldown: 0, animationLock: 0 },
     { id: "10", name: "Raging Strikes", multiplier: 1.2, duration: 20, cooldown: 180, animationLock: 1, image: "raging_strikes.png" },
     { id: "11", name: "Internal Release", multiplier: (1.5 * 1.1), duration: 15, cooldown: 60, animationLock: 1, image: "internal_release.png" }
-  ]
+  ];
 
-  for (let spellObject of spellObjects) {
-    self.spells.push(new Spell(spellObject));
-  }
+  $.each(spellObjects, function(indexInArray, obj) {
+    self.spells.push(new Spell(obj));
+  });
 
-  for (let damageMultiplierAbility of damageMultiplierAbilities) {
-    self.damageMultiplierAbilities.push(new DamageMultiplierAbility(damageMultiplierAbility));
-  }
+  $.each(damageMultiplierAbility, function(indexInArray, obj) {
+    self.damageMultiplierAbilities.push(new DamageMultiplierAbility(obj));
+  });
+
 }
