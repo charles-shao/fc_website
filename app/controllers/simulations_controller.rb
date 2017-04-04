5a0c8ae335d69d341b68c028980dcf143ae55996
Charles Shao <charles@oneflare.com>
2017-04-05 08:01:07 +1000

    Add jobs to simulator

--- app/controllers/simulations_controller.rb
+++ app/controllers/simulations_controller.rb
@@ -1,6 +1,20 @@
 class SimulationsController < ApplicationController
 
   def index
+    load_jobs
   end
-  
+
+  private
+
+    def load_jobs
+      jobs = Job.all.inject([]) do |memo, job|
+        memo.push << {
+          id: job.id,
+          name: job.name
+        }
+      end
+
+      gon.jobs = jobs
+    end
+
 end
