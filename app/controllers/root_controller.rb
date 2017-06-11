19885f74f19c3bd9e16fac81f4984905e099132d
charles <cshao25@gmail.com>
2017-06-11 16:29:30 +1000

    Add static roster to front page

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,10 +1,11 @@
 class RootController < ApplicationController
 
   before_action :require_login, only: :none
-  
+
   def show
     @notices = Notice.most_recent.published.limit(3)
     @static_progressions = StaticProgression.all.includes(:raid_content, :static)
+    @statics = Static.all.includes(:characters)
   end
 
 end
